2736de8d23b2ba0b7d835645fdcbd7e99e272006
LuisFarfan57 <luise.prado99@hotmail.com>
2024-11-02 22:38:13 -0600

    feat: add node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,18 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'hybrid',
+
   experimental: {
     serverIslands: true
-  }
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
